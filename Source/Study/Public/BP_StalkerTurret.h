b8e11951519708188ae4b245d2cdc5e15d63483f
Jawon_LEE <66247624+linstance@users.noreply.github.com>
2026-02-19 18:19:47 +0900

    New Set Up

---
+++ Source/Study/Public/BP_StalkerTurret.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BP_StalkerTurret.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class STUDY_API ABP_StalkerTurret : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABP_StalkerTurret();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* DetectionSphere;
+
+	bool bIsDetected = false;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
